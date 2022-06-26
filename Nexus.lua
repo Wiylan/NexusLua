a9b9d5e11242a627cb1dd4fce1b70bfe4a301744
Wiylan <69250613+Wiylan@users.noreply.github.com>
2022-06-26 23:11:44 +0200

    Add Vehicle to Player Menu

--- Nexus.lua
+++ Nexus.lua
@@ -83,7 +83,7 @@ sportmode = menu.toggle_loop(vehicleMenu, "Sportmode", {"sportmode"}, "Makes you
 	end
 	if PAD.IS_CONTROL_PRESSED(0, 71) then
 		if noGravity then
-			ENTITY.APPLY_FORCE_TO_ENTITY(vehicle, 1, 0.0, sportmodeSpeed, 0.0, 0.0, 0.0, 0.0, 0, 1, 1, 1, 0, 1)
+			ENTITY.APPLY_FORCE_TO_ENTITY(vehicle, 1, 0, sportmodeSpeed, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1)
 		else
 			VEHICLE.SET_VEHICLE_FORWARD_SPEED(vehicle, speed)
 		end
@@ -94,7 +94,7 @@ sportmode = menu.toggle_loop(vehicleMenu, "Sportmode", {"sportmode"}, "Makes you
 			lsp = sportmodeSpeed * 2
 		end
 		if noGravity then
-			ENTITY.APPLY_FORCE_TO_ENTITY(vehicle, 1, 0.0, 0 - (lsp), 0.0, 0.0, 0.0, 0.0, 0, 1, 1, 1, 0, 1)
+			ENTITY.APPLY_FORCE_TO_ENTITY(vehicle, 1, 0, 0 - (lsp), 0, 0, 0, 0, 0, 1, 1, 1, 0, 1)
 		else
 			VEHICLE.SET_VEHICLE_FORWARD_SPEED(vehicle, 0 - (speed))
 		end
@@ -105,9 +105,9 @@ sportmode = menu.toggle_loop(vehicleMenu, "Sportmode", {"sportmode"}, "Makes you
 			lsp = 0 - sportmodeSpeed
 		end
 		if noGravity then
-			ENTITY.APPLY_FORCE_TO_ENTITY(vehicle, 1, (lsp), 0.0, 0.0, 0.0, 0.0, 0.0, 0, 1, 1, 1, 0, 1)
+			ENTITY.APPLY_FORCE_TO_ENTITY(vehicle, 1, (lsp), 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1)
 		else
-			ENTITY.APPLY_FORCE_TO_ENTITY(vehicle, 1, 0 - (speed), 0.0, 0.0, 0.0, 0.0, 0.0, 0, 1, 1, 1, 0, 1)
+			ENTITY.APPLY_FORCE_TO_ENTITY(vehicle, 1, 0 - (speed), 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1)
 		end
 	end
 	if PAD.IS_CONTROL_PRESSED(0, 64) then
@@ -116,18 +116,18 @@ sportmode = menu.toggle_loop(vehicleMenu, "Sportmode", {"sportmode"}, "Makes you
 			lsp = sportmodeSpeed * 2
 		end
 		if noGravity then
-			ENTITY.APPLY_FORCE_TO_ENTITY(vehicle, 1, lsp, 0.0, 0.0, 0.0, 0.0, 0.0, 0, 1, 1, 1, 0, 1)
+			ENTITY.APPLY_FORCE_TO_ENTITY(vehicle, 1, lsp, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1)
 		else
-			ENTITY.APPLY_FORCE_TO_ENTITY(vehicle, 1, speed, 0.0, 0.0, 0.0, 0.0, 0.0, 0, 1, 1, 1, 0, 1)
+			ENTITY.APPLY_FORCE_TO_ENTITY(vehicle, 1, speed, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1)
 		end
 	end
 	if not noGravity and not PAD.IS_CONTROL_PRESSED(0, 71) and not PAD.IS_CONTROL_PRESSED(0, 72) then
-		VEHICLE.SET_VEHICLE_FORWARD_SPEED(vehicle, 0.0)
+		VEHICLE.SET_VEHICLE_FORWARD_SPEED(vehicle, 0)
 	end
 	if TASK.GET_IS_TASK_ACTIVE(players.user_ped(), 2) then
 		menu.trigger_command(sportmode, "off")
 		if stopOnExit then
-			VEHICLE.SET_VEHICLE_FORWARD_SPEED(vehicle, 0.0)
+			VEHICLE.SET_VEHICLE_FORWARD_SPEED(vehicle, 0)
 		end
 	end
 end, function()
@@ -156,13 +156,13 @@ menu.toggle(sportmodeMenu, "Stop on Exit", {}, "", function(toggle)
 	stopOnExit = toggle
 end)
 
-menu.divider(vehicleMenu, "-- Miscellaneous --")
+menu.divider(vehicleMenu, "Miscellaneous")
 
 menu.toggle_loop(vehicleMenu, "Low Traction", {"driftmode"}, "Ideal for drifting.\nSetting a hotkey is recommended.", function()
 	local vehicle <const> = PED.GET_VEHICLE_PED_IS_IN(players.user_ped(), false)
 	if NETWORK.NETWORK_HAS_CONTROL_OF_ENTITY(vehicle) then
 		VEHICLE.SET_VEHICLE_REDUCE_GRIP(vehicle, true)
-		--VEHICLE._SET_VEHICLE_REDUCE_TRACTION(vehicle, 0.0)
+		--VEHICLE._SET_VEHICLE_REDUCE_TRACTION(vehicle, 0)
 		if TASK.GET_IS_TASK_ACTIVE(players.user_ped(), 2) then
 			VEHICLE.SET_VEHICLE_REDUCE_GRIP(vehicle, false)
 		end
@@ -173,3 +173,92 @@ end, function()
 		VEHICLE.SET_VEHICLE_REDUCE_GRIP(vehicle, false)
 	end
 end)
+
+--[[
+----------------------
+--- Player Options ---
+----------------------
+--]]
+
+function controlVehicle(pid)
+	local pos <const> = players.get_position(players.user())
+	local targetPos <const> = players.get_position(pid)
+	local distance <const> = SYSTEM.VDIST2(targetPos.x, targetPos.y, 0, pos.x, pos.y, 0)
+	local targetVehicle = PED.GET_VEHICLE_PED_IS_IN(PLAYER.GET_PLAYER_PED(pid), true)
+	local spectate <const> = menu.ref_by_rel_path(menu.player_root(pid), "Spectate>Ninja Method")
+	local spectating <const> = menu.get_value(spectate)
+
+	if targetVehicle == 0 and distance > 340000 then
+		local timeout <const> = os.time() + 3
+		menu.trigger_command(spectate, "on")
+		while targetVehicle == 0 and timeout > os.time() do
+			targetVehicle = PED.GET_VEHICLE_PED_IS_IN(PLAYER.GET_PLAYER_PED(pid), true)
+			util.yield(100)
+		end
+	end
+	if targetVehicle > 0 then
+		if NETWORK.NETWORK_HAS_CONTROL_OF_ENTITY(targetVehicle) then
+			return targetVehicle
+		end
+		NETWORK.SET_NETWORK_ID_CAN_MIGRATE(NETWORK.NETWORK_GET_NETWORK_ID_FROM_ENTITY(targetVehicle), true)
+		local hasControl = false
+		local loop = 15
+		while not hasControl and loop > 0 do
+			hasControl = NETWORK.NETWORK_REQUEST_CONTROL_OF_ENTITY(targetVehicle)
+			loop -= 1
+			util.yield(15)
+		end
+	end
+	if not spectating then
+		menu.trigger_command(spectate, "off")
+	end
+	if targetVehicle == 0 then
+		util.toast(players.get_name(pid) .. " is not in a Vehicle. :/")
+	end
+	return targetVehicle
+end
+
+function playerRoot(pid)
+	menu.divider(menu.player_root(pid), "-- Nexus --")
+
+	local playerVehicleMenu <const> = menu.list(menu.player_root(pid), "Vehicle", {}, "Functionality depends on network conditions.")
+	menu.divider(playerVehicleMenu, "-- Vehicle --")
+
+	menu.action(playerVehicleMenu, "Fix", {}, "", function()
+		local vehicle <const> = controlVehicle(pid)
+		VEHICLE.SET_VEHICLE_FIXED(vehicle)
+		VEHICLE.SET_VEHICLE_DEFORMATION_FIXED(vehicle)
+		VEHICLE.SET_VEHICLE_DIRT_LEVEL(vehicle, 0)
+	end)
+	menu.action(playerVehicleMenu, "Tune", {}, "", function()
+		local vehicle <const> = controlVehicle(pid)
+		VEHICLE.SET_VEHICLE_MOD_KIT(vehicle, 0)
+		for i = 0, 50 do
+			VEHICLE.SET_VEHICLE_MOD(vehicle, i, VEHICLE.GET_NUM_VEHICLE_MODS(vehicle, i) - 1, false)
+		end
+	end)
+	menu.action(playerVehicleMenu, "Kill Engine", {}, "", function()
+		local vehicle <const> = controlVehicle(pid)
+		VEHICLE.SET_VEHICLE_ENGINE_HEALTH(vehicle, -4000)
+	end)
+	menu.action(playerVehicleMenu, "Burst Tires", {}, "", function()
+		local vehicle <const> = controlVehicle(pid)
+		VEHICLE.SET_VEHICLE_TYRES_CAN_BURST(vehicle, true)
+		for i = 0, 7 do
+			VEHICLE.SET_VEHICLE_TYRE_BURST(vehicle, i, true, 1000)
+		end
+	end)
+	menu.action(playerVehicleMenu, "Catapult", {}, "", function()
+		local vehicle <const> = controlVehicle(pid)
+		--AUDIO.SET_VEHICLE_BOOST_ACTIVE(vehicle, true)
+		ENTITY.APPLY_FORCE_TO_ENTITY(vehicle, 1, 0, 0, 9999, 0, 0, 0, 1, false, true, true, true, true)
+		--AUDIO.SET_VEHICLE_BOOST_ACTIVE(vehicle, false)
+	end)
+	menu.action(playerVehicleMenu, "Delete", {}, "", function()
+		local vehicle <const> = controlVehicle(pid)
+		entities.delete_by_handle(vehicle)
+	end)
+end
+
+players.on_join(playerRoot)
+players.dispatch_on_join()
